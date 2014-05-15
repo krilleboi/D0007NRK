05a3bf767ef185c2e43e5492440651a142f77c26
Rikard <Rikard@SHODAN>
2014-05-15 15:13:49 +0200

    Add Task har uppdaterats med en bonnalösning för att lägga till ett case

--- DesktopApp/src/desktopapp/AddTask.java
+++ DesktopApp/src/desktopapp/AddTask.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 /**
  *
@@ -24,37 +24,12 @@ public class AddTask extends javax.swing.JFrame {
     /**
      * Creates new form AddTask
      */
+    
+    
     public AddTask() {
-        indexCategories();
         initComponents();
     }
-    private ArrayList<String> CaseId = new ArrayList<>();
-    private ArrayList<String> BudgetedTime = new ArrayList<>();
  
-    AddTask(String username) {
-        this.UserName = username;
-        
-        indexCategories();
-        initComponents();        
-    }
- 
-   
-    private void indexCategories() {
-        //gets the data from the category table
- 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://aasa.asuscomm.com:3306/d0007nrk", "d0007nrk", "d0007nrk");
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select * from 'Case'");
-            while (rs.next()) {
-                CaseId.add(rs.getString("CaseId"));
-                BudgetedTime.add(rs.getString("BudgetedTime"));
-            }
-        } catch (ClassNotFoundException | SQLException ex) {
-            System.out.println("Exception occur :" + ex);
-        }
-    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -64,10 +39,15 @@ public class AddTask extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        bindingGroup = new org.jdesktop.beansbinding.BindingGroup();
 
         entityManager = java.beans.Beans.isDesignTime() ? null : javax.persistence.Persistence.createEntityManagerFactory("d0007nrk?zeroDateTimeBehavior=convertToNullPU").createEntityManager();
         caseQuery = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT c FROM Case c");
         caseList = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : caseQuery.getResultList();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        caseQuery1 = java.beans.Beans.isDesignTime() ? null : entityManager.createQuery("SELECT c FROM Case c");
+        caseList1 = java.beans.Beans.isDesignTime() ? java.util.Collections.emptyList() : caseQuery1.getResultList();
         jLabel1 = new javax.swing.JLabel();
         SaveButton = new javax.swing.JButton();
         TaskField = new javax.swing.JTextField();
@@ -80,7 +60,25 @@ public class AddTask extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         StatusBox = new javax.swing.JComboBox();
         jLabel7 = new javax.swing.JLabel();
-        CaseIdBox = new javax.swing.JComboBox();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        CaseTable = new javax.swing.JTable();
+        CaseIdField = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        AddNewTask = new javax.swing.JButton();
+        ReturnCaseListButton = new javax.swing.JButton();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -112,7 +110,34 @@ public class AddTask extends javax.swing.JFrame {
 
         jLabel7.setText("Case");
 
-        CaseIdBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        org.jdesktop.swingbinding.JTableBinding jTableBinding = org.jdesktop.swingbinding.SwingBindings.createJTableBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, caseList1, CaseTable);
+        org.jdesktop.swingbinding.JTableBinding.ColumnBinding columnBinding = jTableBinding.addColumnBinding(org.jdesktop.beansbinding.ELProperty.create("${caseId}"));
+        columnBinding.setColumnName("Case Id");
+        columnBinding.setColumnClass(Integer.class);
+        bindingGroup.addBinding(jTableBinding);
+        jTableBinding.bind();
+        CaseTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                CaseTableMouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(CaseTable);
+
+        jLabel6.setText("Created Cases");
+
+        AddNewTask.setText("Add new task");
+        AddNewTask.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AddNewTaskActionPerformed(evt);
+            }
+        });
+
+        ReturnCaseListButton.setText("Return to Case list");
+        ReturnCaseListButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ReturnCaseListButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,21 +145,38 @@ public class AddTask extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(22, 22, 22)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel1)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(SaveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING))
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(TaskField)
-                    .addComponent(StatusBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-                    .addComponent(CategoryBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(CaseIdBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(211, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 21, Short.MAX_VALUE)
+                        .addComponent(jLabel3)
+                        .addGap(494, 494, 494))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(CaseIdField, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(jLabel1)
+                                        .addComponent(TaskField)
+                                        .addComponent(StatusBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+                                        .addComponent(CategoryBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jLabel7))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(47, 47, 47))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(SaveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(AddNewTask, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(ReturnCaseListButton)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -157,14 +199,23 @@ public class AddTask extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel7)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(CaseIdBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
-                .addComponent(SaveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(CaseIdField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(SaveButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(ReturnCaseListButton, javax.swing.GroupLayout.DEFAULT_SIZE, 35, Short.MAX_VALUE)
+                    .addComponent(AddNewTask, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
+
+        bindingGroup.bind();
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -176,7 +227,7 @@ public class AddTask extends javax.swing.JFrame {
         
         
         try {
-            String sql = "Insert into `Task`(Task,Status,Category,Comment,Case_CaseId)values(?,?,?,?,?)";
+            String sql = "Insert into Task (Task,Status,Category,Description,Case_CaseId)values(?,?,?,?,?)";
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://aasa.asuscomm.com:3306/d0007nrk","d0007nrk","d0007nrk");
             PreparedStatement t= con.prepareStatement(sql);
@@ -185,16 +236,7 @@ public class AddTask extends javax.swing.JFrame {
             t.setString(2, StatusBox.getSelectedItem().toString());
             t.setString(3, CategoryBox.getSelectedItem().toString());
             t.setString(4, DescritionArea.getText());
-            
-            //converts categoryNames to categoryID
-            String CId = null;
-            for (int i = 0; i < BudgetedTime.size(); i++) {
-                int test = 000;
-                if (BudgetedTime.get(i).equals(CaseIdBox.getSelectedItem().toString())) {
-                    CId = (CaseId.get(i));
-                }
-            }
-            t.setString(5, CId);
+            t.setString(5, CaseIdField.getText());
             t.execute();
             
             JOptionPane.showMessageDialog(null, "You have saved your Task!");
@@ -207,6 +249,22 @@ public class AddTask extends javax.swing.JFrame {
           System.out.println("Exception has occured : " + ex);
         }
     }//GEN-LAST:event_SaveButtonActionPerformed
+
+    private void CaseTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CaseTableMouseClicked
+     
+    }//GEN-LAST:event_CaseTableMouseClicked
+
+    private void AddNewTaskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddNewTaskActionPerformed
+       AddTask a= new AddTask();
+              a.setVisible(true);
+              dispose();
+    }//GEN-LAST:event_AddNewTaskActionPerformed
+
+    private void ReturnCaseListButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReturnCaseListButtonActionPerformed
+        ListCases l= new ListCases();
+              l.setVisible(true);
+              dispose();
+    }//GEN-LAST:event_ReturnCaseListButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -244,21 +302,31 @@ public class AddTask extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox CaseIdBox;
+    private javax.swing.JButton AddNewTask;
+    private javax.swing.JTextField CaseIdField;
+    private javax.swing.JTable CaseTable;
     private javax.swing.JComboBox CategoryBox;
     private javax.swing.JTextArea DescritionArea;
+    private javax.swing.JButton ReturnCaseListButton;
     private javax.swing.JButton SaveButton;
     private javax.swing.JComboBox StatusBox;
     private javax.swing.JTextField TaskField;
     private java.util.List<desktopapp.Case> caseList;
+    private java.util.List<desktopapp.Case> caseList1;
     private javax.persistence.Query caseQuery;
+    private javax.persistence.Query caseQuery1;
     private javax.persistence.EntityManager entityManager;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private org.jdesktop.beansbinding.BindingGroup bindingGroup;
     // End of variables declaration//GEN-END:variables
 }
